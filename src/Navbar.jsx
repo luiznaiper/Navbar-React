27d9e1b30b5d0de7ee40940c41a3ac93ab9a78a4
luiznaiper <luiz_naiper@hotmail.com>
2023-04-14 06:33:57 -0300

    :construction: Almost done navbar with useRef

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { FaBars } from 'react-icons/fa'
 import { links } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
 
   const toggleLinks = () => {
     setShowLinks(!showLinks)
+  }
+
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : '0px',
   }
 
   return (
@@ -20,11 +28,11 @@ const Navbar = () => {
           </button>
         </div>
         <div
-          className={
-            showLinks ? 'links-container show-container' : 'links-container'
-          }
+          className="links-container"
+          ref={linksContainerRef}
+          style={linkStyles}
         >
-          <ul className="links">
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link
               return (
